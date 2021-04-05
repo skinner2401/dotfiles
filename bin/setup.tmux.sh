2846cbdf55b6edc6c04e8f8185f24e59c51ee036
Chris Amundson <christopher.b.amundson@gmail.com>
2021-04-05 11:56:30 -0700

    various improvements

--- bin/setup.tmux.sh
+++ bin/setup.tmux.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-rm -rf ~/src/tmux
-git clone https://github.com/gpakosz/.tmux.git ~/src/tmux
-ln -s -f ~/src/tmux/.tmux.conf ~/.tmux.conf
-cp ~/src/tmux/.tmux.conf.local ~/.tmux.conf.local
+INSTALLDIR=$HOME/src
+if ! ( ls $INSTALLDIR) then
+  mkdir $INSTALLDIR
+fi
 
-cat ~/src/dotfiles/.tmux.conf >> ~/.tmux.conf.local
+if ( ls $INSTALLDIR/tmux ) then 
+  rm -rf $INSTALLDIR/tmux
+fi
+
+git clone https://github.com/gpakosz/.tmux.git $INSTALLDIR/tmux
+ln -s -f $INSTALLDIR/tmux/.tmux.conf ~/.tmux.conf
+cp $INSTALLDIR/tmux/.tmux.conf.local ~/.tmux.conf.local
+
+cat $INSTALLDIR/dotfiles/.tmux.conf >> ~/.tmux.conf.local
