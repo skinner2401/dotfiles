c7a40f7e55bc440788c32bf34c169ac2713f26ee
Chris Amundson <christopher.b.amundson@gmail.com>
2022-04-20 12:34:33 -0700

    Add zshrc symlink setup

--- bin/setup-links.sh
+++ bin/setup-links.sh
@@ -6,29 +6,32 @@ NOW=`date '+%F_%H-%M-%S'`
 SCRIPTDIR=${0:a:h}
 echo "script dir: $SCRIPTDIR"
 
+DF=$SCRIPTDIR/..
 CFG=$HOME/.config
-VIMRCDF=$SCRIPTDIR/../.vimrc
+VIMRCDF=$DF/.vimrc
 NVIMCONFDIR=$CFG/nvim
 NVIMCONF=$NVIMCONFDIR/init.vim
-TMUXCONFDF=$SCRIPTDIR/../.tmux.conf
+TMUXCONFDF=$DF/.tmux.conf
 TMUXCONF=$HOME/.tmux.conf
+ZSHRCDF=$DF/.zshrc
+ZSHRC=$HOME/.zshrc
 
 if [ -d "$CFG" ]; then 
 echo "backing up $CFG"
-    CFGBKUP=$CFG.$NOW.bkup
-    echo "backing up $CFG to $CFGBKUP"
-    mv $CFG $CFGBKUP
+  CFGBKUP=$CFG.$NOW.bkup
+  echo "backing up $CFG to $CFGBKUP"
+  mv $CFG $CFGBKUP
 fi
 
 if ! [ -d "$CFG" ]; then
-    echo "no $NVIMCONFDIR dir exists, creating"
-    mkdir -p $NVIMCONFDIR
+  echo "no $NVIMCONFDIR dir exists, creating"
+  mkdir -p $NVIMCONFDIR
 fi
 
 if [ -f "$TMUXCONF" ]; then
-    TMUXBKUP=$TMUXCONF.$NOW.bkup
-    echo "backing up $TMUXCONF to $TMUXBKUP"
-    mv $TMUXCONF $TMUXBKUP
+  TMUXBKUP=$TMUXCONF.$NOW.bkup
+  echo "backing up $TMUXCONF to $TMUXBKUP"
+  mv $TMUXCONF $TMUXBKUP
 fi
 
 echo "creating tmux conf symlink from \"$TMUXCONFDF\" to $TMUXCONF"
@@ -36,3 +39,12 @@ ln -s $TMUXCONFDF $TMUXCONF
 
 echo "creating nvim conf symlinks from \"$VIMRCDF\" to $NVIMCONF"
 ln -s $VIMRCDF $NVIMCONF
+
+if [ -f "$ZSHRC" ]; then
+  ZSHRCBKUP=$ZSHRC.$NOW.bkup
+  echo "backing up $ZSHRC to $ZSHRCBKUP"
+  mv $ZSHRC $ZSHRCBKUP
+fi
+ 
+echo "creating zshrc symlink"
+ln -s $ZSHRCDF $ZSHRC
