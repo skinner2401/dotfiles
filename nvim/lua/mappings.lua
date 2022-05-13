fe6aa76fd79c023d4dd22e88fac56397590080a5
Chris Amundson <christopher.b.amundson@gmail.com>
2022-05-13 04:36:01 -0400

    fix c-o navigation clearing tmux buffer

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,6 +1,11 @@
 vim.api.nvim_set_keymap( 'n', '<C-e>', ':NERDTreeToggle<CR>', {})
 vim.api.nvim_set_keymap( 'n', ',<space>', ':nohlsearch<CR>', { silent = true })
-vim.api.nvim_set_keymap( 'n', '<C-A>', ':source ~/.config/nvim/init.lua<CR>:PlugInstall<CR>', { nowait = true })
+vim.api.nvim_set_keymap( 'n', '<C-A>', ':source ~/.config/nvim/init.lua<CR>:PlugInstall<CR>:PlugClean<CR>', { nowait = true })
 vim.api.nvim_set_keymap( 'n', '<C-p>', ':FZF<CR>', { nowait = true, silent = true })
 
-
+-- vim <-> tmux navigation
+vim.cmd('let g:tmux_navigator_no_mappings = 1')
+vim.api.nvim_set_keymap('n', '<C-h>', ":TmuxNavigateLeft<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', ":TmuxNavigateDown<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ":TmuxNavigateUp<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', ":TmuxNavigateRight<cr>", { noremap = true, silent = true })
--- nvim/lua/vimplug.lua
+++ nvim/lua/vimplug.lua
@@ -1,47 +1,18 @@
 vim.cmd([[
-"setup vim-plug {{{
-
-  "Note: install vim-plug if not present
-  if empty(glob('~/.config/nvim/autoload/plug.vim'))
-    silent !curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    autocmd VimEnter * PlugInstall
-  endif
-
-  "Note: Skip initialization for vim-tiny or vim-small.
-  if !1 | finish | endif
-  if has('vim_starting')
-    set nocompatible               " Be iMproved
-    " Required:
-    call plug#begin()
-  endif
-
-"}}}
-
-" installs vim-plug
-if empty(glob('~/.vim/autoload/plug.vim'))
-  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
-    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
-endif
 
 call plug#begin('~/.vim/plugged')
 
-" Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
-"Plug 'junegunn/vim-easy-align'
 Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
 Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-Plug 'junegunn/fzf.vim'"
+Plug 'junegunn/fzf.vim'
 
 Plug 'itchyny/lightline.vim' " ~/.vimrc.lightline
 
 Plug 'vim-scripts/bash-support.vim' " bash linting
-Plug 'ParamagicDev/vim-medic_chalk'
 Plug 'artanikin/vim-synthwave84'
 Plug 'semibran/vim-colors-synthetic'
-"Plug 'typescript-eslint/typescript-eslint'
-"Plug 'sheerun/vim-polyglot'
-"Plug 'neoclide/coc.nvim'
 Plug 'christoomey/vim-tmux-navigator'
+"Plug 'alexghergh/nvim-tmux-navigation'
 Plug 'tpope/vim-fugitive'
 Plug 'folke/tokyonight.nvim', { 'branch': 'main' }
 Plug 'neovim/nvim-lspconfig'
@@ -53,9 +24,8 @@ Plug 'hrsh7th/nvim-cmp'
 
 Plug 'L3MON4D3/LuaSnip'
 
-
 " adds eslint capability to vim
-Plug 'dense-analysis/ale'
+"Plug 'dense-analysis/ale'
 
 call plug#end()
 
