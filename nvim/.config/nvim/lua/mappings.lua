3c80a6f5cb254e4766d025fc7c79ba3fd48395f6
Chris Amundson <christopher.b.amundson@gmail.com>
2022-06-20 03:02:32 -0400

    fix: use gitignore for default c-p

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,7 +1,8 @@
 vim.api.nvim_set_keymap( 'n', '<C-e>', ':NERDTreeToggle<CR>', {})
 vim.api.nvim_set_keymap( 'n', ',<space>', ':nohlsearch<CR>', { silent = true })
 vim.api.nvim_set_keymap( 'n', '<C-A>', ':source ~/.config/nvim/init.lua<CR>:PlugInstall<CR>:PlugClean<CR>', { nowait = true })
-vim.api.nvim_set_keymap( 'n', '<C-p>', ':FZF<CR>', { nowait = true, silent = true })
+vim.api.nvim_set_keymap( 'n', '<C-p>', ':GFiles<CR>', { nowait = true, silent = true })
+vim.api.nvim_set_keymap( 'n', '<Leader><C-p>', ':FZF<CR>', { nowait = true, silent = true })
 vim.api.nvim_set_keymap( 'n', '<Leader>e', ':Ex<CR>', { nowait = true, silent = true })
 
 -- vim <-> tmux navigation
