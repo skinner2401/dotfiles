ea88ee221993711fdc7e1659e0479f09d8c2a49a
Chris Amundson <christopher.b.amundson@gmail.com>
2022-06-20 02:43:54 -0400

    feat: add neovim rust server config

--- nvim/.config/nvim/lua/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lsp/handlers.lua
@@ -79,3 +79,20 @@ require('lspconfig')['eslint'].setup{
   }
 }
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+require('lspconfig')['rust_analyzer'].setup({
+    capabilities=capabilities,
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+        -- to enable rust-analyzer settings visit:
+        -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+        ["rust-analyzer"] = {
+            -- enable clippy diagnostics on save
+            checkOnSave = {
+                command = "clippy"
+            },
+        }
+    }
+})
