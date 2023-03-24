94d602b34b9c64929da42f2fa0c2efb17cdffe16
Chris Amundson <christopher.b.amundson@gmail.com>
2023-03-23 22:04:58 -0700

    chore: add hover for lsp

--- nvim/.config/nvim/lua/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lsp/handlers.lua
@@ -92,7 +92,6 @@ local cmp = require'cmp'
 local opts = {
   tools = { -- rust-tools options
     autoSetHints = true,
-    hover_with_actions = true,
     inlay_hints = {
       show_parameter_hints = true,
       parameter_hints_prefix = "",
