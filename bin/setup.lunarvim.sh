11dbf4f2411d30591df67073fe89ce7b770c2486
Chris Amundson <christopher.b.amundson@gmail.com>
2023-03-26 14:11:23 -0700

    fix: lunarvim setup script require neovim

--- bin/setup.lunarvim.sh
+++ bin/setup.lunarvim.sh
@@ -2,15 +2,61 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-scriptdir="$( dirname -- "$BASH_SOURCE"; )";
-. $scriptdir/setup.coreutils.sh
+echo Linux coreutils setup start.
 
-#SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-#REPOHOME=$SCRIPT_DIR/..
-#NOW=`date +%s`
-#LUADFCFGDIR=$($REALPATHBIN $REPOHOME/nvim)
-#SRCDIR=$HOME/src
+if [ -z 'realpath 2>/dev/null' ]; then
+  if which grealpath ; then
+    REALPATHBIN=grealpath
+  else
+    echo 'please install linux core utils, macos: `brew install coreutils`'
+    exit 1
+  fi
+else
+  REALPATHBIN=realpath
+fi
 
-#. $scriptdir/setup.neovim.sh
+echo coreutils setup complete.
+
+if ! which realpath ; then
+  if which grealpath ; then
+    REALPATHBIN=grealpath
+  else
+    echo 'please install linux core utils, macos: `brew install coreutils`'
+    exit 1
+  fi
+else
+  REALPATHBIN=realpath
+fi
+
+NOW=`date +%s`
+SRCDIR=$HOME/src
+
+if ! hash nvim; then
+  echo 'neovim not detetcted, installing neovim'
+  echo 'cloning neovim'
+  git clone git@github.com:neovim/neovim.git $SRCDIR/neovim
+  pushd $SRCDIR/neovim
+  
+  echo 'Fetching v0.8.3'
+  git fetch origin v0.8.3
+  echo 'checking out v0.8.3'
+  git checkout v0.8.3
+  echo 'Installing neovim dependencies.'
+  sudo apt-get update
+  sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+  make CMAKE_BUILD_TYPE=RelWithDebInfo
+  echo 'Working directory:'
+  pwd
+  echo 'calling make install'
+  sudo make install
+else
+  echo 'neovim exists. Not installing.'
+fi
+
+# install vimplug if it isn't already
+if [ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ] ; then
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
 
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
