3c2ad237e14d93cbccf847bb20ae093015879096
Chris Amundson <christopher.b.amundson@gmail.com>
2023-03-26 19:31:34 -0700

    fix: further automate lunarvim setup

--- bin/setup.lunarvim.sh
+++ bin/setup.lunarvim.sh
@@ -2,36 +2,36 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+function pressanykey() {
+  echo "Press any key to continue..."
+  read -n 1 k <&1
+}
+
+pressanykey
+echo "Checking for ensurepip (python3-pip)"
+if ! command -v ensurepip ; then
+  echo 'python3-pip not detected, installing python3-pip'
+  sudo apt-get install -y python3-pip
+fi
+
 echo Linux coreutils setup start.
 
-if [ -z 'realpath 2>/dev/null' ]; then
-  if which grealpath ; then
-    REALPATHBIN=grealpath
-  else
+pressanykey
+if ! command -v realpath ; then
+  if ! command -v grealpath ; then
     echo 'please install linux core utils, macos: `brew install coreutils`'
-    exit 1
+    sudo apt-get install -y coreutils
+  else
+    REALPATHBIN=grealpath
   fi
 else
   REALPATHBIN=realpath
 fi
 
-echo coreutils setup complete.
-
-if ! which realpath ; then
-  if which grealpath ; then
-    REALPATHBIN=grealpath
-  else
-    echo 'please install linux core utils, macos: `brew install coreutils`'
-    exit 1
-  fi
-else
-  REALPATHBIN=realpath
-fi
-
-NOW=`date +%s`
 SRCDIR=$HOME/src
 
-if ! hash nvim; then
+pressanykey
+if ! command -v nvim ; then
   echo 'neovim not detetcted, installing neovim'
   echo 'cloning neovim'
   git clone https://github.com/neovim/neovim.git $SRCDIR/neovim
@@ -53,10 +53,18 @@ else
   echo 'neovim exists. Not installing.'
 fi
 
+if ! command -v cargo ; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  source "$HOME/.cargo/env"
+fi
+
 # install vimplug if it isn't already
 if [ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ] ; then
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
-
-LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+export ARGS_INSTALL_DEPENDENCIES=1
+export INTERACTIVE_MODE=0
+export LV_BRANCH='release-1.2/neovim-0.8'
+pressanykey
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
