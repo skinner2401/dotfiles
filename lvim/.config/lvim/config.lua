98012f47695e9fddb5873ba1b15efac43bb04c1f
Chris Amundson <christopher.b.amundson@gmail.com>
2023-05-04 22:10:05 -0700

    feat: enable win32yank.exe clipboard integration for wsl2

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -373,22 +373,22 @@ lvim.builtin.dap.on_config_done = function(dap)
   --}
 end
 
---if vim.fn.has("wsl") == 1 then
---  vim.api.nvim_exec(
---    [[
---    let g:clipboard = {
---        \   'name': 'win32yank-wsl',
---        \   'copy': {
---        \      '+': 'win32yank.exe -i --crlf',
---        \      '*': 'win32yank.exe -i --crlf',
---        \    },
---        \   'paste': {
---        \      '+': 'win32yank.exe -o --lf',
---        \      '*': 'win32yank.exe -o --lf',
---        \   },
---        \   'cache_enabled': 0,
---        \ }
---  ]],
---    true
---  )
---end
+if vim.fn.has("wsl") == 1 then
+  vim.api.nvim_exec(
+    [[
+    let g:clipboard = {
+        \   'name': 'win32yank-wsl',
+        \   'copy': {
+        \      '+': 'win32yank.exe -i --crlf',
+        \      '*': 'win32yank.exe -i --crlf',
+        \    },
+        \   'paste': {
+        \      '+': 'win32yank.exe -o --lf',
+        \      '*': 'win32yank.exe -o --lf',
+        \   },
+        \   'cache_enabled': 0,
+        \ }
+  ]],
+    true
+  )
+end
