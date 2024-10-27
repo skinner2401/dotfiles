a2a046416bb92419c25712b52c8064e8fbc5fe4e
Chris Amundson <skinner@Amandas-MacBook-Pro.local>
2024-10-27 13:29:42 -0700

    WIP split out between macos (zsh) and pios

---
+++ nvim/.config/nvim/lua/lazysetup.lua
@@ -0,0 +1,56 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  -- highlight-start
+  spec = {
+    -- add your plugins here
+    'scrooloose/nerdtree',
+    'junegunn/fzf',
+    'junegunn/fzf.vim',
+    'itchyny/lightline.vim',
+    'semibran/vim-colors-synthetic',
+    'christoomey/vim-tmux-navigator',
+    'tpope/vim-fugitive',
+    'folke/tokyonight.nvim',
+    'williamboman/nvim-lsp-installer',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+    'hrsh7th/vim-vsnip',
+    'hrsh7th/cmp-vsnip',
+    'simrat39/rust-tools.nvim',
+    'ray-x/lsp_signature.nvim'
+  },
+  -- highlight-end
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
